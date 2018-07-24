dd565d007a9a685d26cad9c61ded2b623a14b980
Schuyler.Jones <Schuyler.Jones@FNFCORPTECWK809.fnfglobal.local>
2018-07-24 16:20:40 -0400

    creation of story 71622

---
+++ Scripts/71622_leverage Auth0 hosted login pages/72975_Verify TitlePayPlus login page is the same as ATX, MTX Auth0/Script1532457636842.groovy
@@ -0,0 +1,50 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+'Launch Browser'
+WebUI.openBrowser('')
+
+'Navigate to TPP URL '
+WebUI.navigateToUrl(findTestData('Application').getValue(1, 1))
+
+'Wait for element <Log In> Link'
+WebUI.waitForElementPresent(findTestObject('Page_FNF - TitlePay Plus/a_Log In'), 20)
+
+'Click <Login> Link'
+WebUI.click(findTestObject('Page_FNF - TitlePay Plus (Invoices)/a_Log In'))
+
+'Wait for Button <Login>'
+WebUI.waitForElementPresent(findTestObject('Page_FNTG Login/button_Log In'), 20)
+
+'Verify TPP URL redirect to Auth0 Hosted page'
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_FNTG Login/auth0_h3_Welcome to the FNTG Agency'), 0)
+
+'Navigate to ATX URL '
+WebUI.navigateToUrl(findTestData('Application').getValue(1, 3))
+
+'Wait for Button <Login>'
+WebUI.waitForElementPresent(findTestObject('Page_FNTG Login/button_Log In'), 20)
+
+'Verify same login page is displayed'
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_FNF - Atx Login/h3_Welcome to the FNTG Agency'), 0)
+
+'Close Browser'
+WebUI.closeBrowser()
